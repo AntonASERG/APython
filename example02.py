441f182467155df390d88fce2d950a8904949660
Anton Anisimov <seemannrr@ya.ru>
2022-09-25 15:04:55 +0300

    sem

---
+++ example02.py
@@ -0,0 +1,33 @@
+# Задача 2. Принимает 5 чисел, выдает МАХ и не только
+
+# Задать пустой массив 
+list = []
+
+# Цикл чтобы заполнить массив
+for i in range(5): #(ot 2, do 5, IIIar 2)
+    list.append(int(input(f"Введите {i+1} число - ")))
+# перебрать по индексу
+for t in range (len(num_array)):
+    print (t)
+    print (f"Элемент {num_array[t]} имеет индекс {t}")
+# перебрать и пезаписать вручную
+for t in range (len(num_array)):
+    num_array[i] = input()
+
+# цикл найти макс
+max = list[0]
+for i in list:
+    if i > max:
+
+print(max)
+    
+# 
+for elem in range (len(num_array)):
+    print (elem)
+    print (f"{num_array[elem]} имеет индекс {elem}")
+
+
+
+
+
+
