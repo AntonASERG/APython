177af86471baab7682f149fa4caa4303cc93cad9
Anton <seemannRR@ya.ru>
2022-10-10 15:22:35 +0300

    51 52

---
+++ DZ5/DZ5_1.py
@@ -0,0 +1,26 @@
+# DZ 5 - 1. Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+
+strText = input ('Введите текст через пробел:  ')
+print (strText)
+
+# метод принимает строку, требует ввода фильтра 
+# и возвращает строку с элементами не содержащими фильтр
+def elemFilter (str):
+
+    listText = str.split()
+    strElem = input ('Введите элемент для поиска и удаления:  ')
+    print (strElem)
+
+    listCut = []
+    strCut = ''
+
+    for i in range(len(listText)):
+        if listText[i].find(strElem) == -1:
+            listCut.append(listText[i])
+    strCut = ' '.join(listCut)
+    return strCut
+
+print(elemFilter(strText))
+
+
